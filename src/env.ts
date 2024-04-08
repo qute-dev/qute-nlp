910698f1654326beb53a034fb42296bd2795f3ca
M Dedi Rudianto <mdedirudianto@gmail.com>
2024-04-08 21:37:06 +0700

    feat: add meili search lib

--- src/env.ts
+++ src/env.ts
@@ -3,6 +3,9 @@ export const env = {
   DEBUG: process.env.DEBUG === 'false',
   HOST: process.env.HOST || '127.0.0.1',
   PORT: process.env.PORT || 7777,
+  MEILI_ADDRESS: process.env.MEILI_ADDRESS || 'http://127.0.0.1:7700',
+  MEILI_KEY:
+    process.env.MEILI_KEY || 'xck1bMOeroIaAKS_P0giqoVKxwUGsZ_iHM7qgd32EK4',
 };
 
 export default env;
