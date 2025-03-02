9d8fd8d74f58640750b858f95fb17b6a0204724d
mdedirudianto <mdedirudianto@gmail.com>
2025-03-02 10:16:30 +0700

    chore: build lib

--- lib/bot/verse.d.ts
+++ lib/bot/verse.d.ts
@@ -1,4 +1,5 @@
 import { Answer } from '../models';
 export declare function getVerseRange(chapterNo: number, verseStart: number, verseEnd: number): Answer;
 export declare function getVersesByIds(verseIds: number[]): Answer;
+export declare function getRandomVerse(): Answer;
 //# sourceMappingURL=verse.d.ts.map
--- lib/models/answer.d.ts
+++ lib/models/answer.d.ts
@@ -1,5 +1,5 @@
 import { Chapter, Verse } from 'qute-corpus';
-export type ActionType = 'search' | 'index' | 'next' | 'greeting' | 'none';
+export type ActionType = 'search' | 'index' | 'next' | 'greeting' | 'random' | 'none';
 export type SourceType = 'quran' | 'tafsir' | 'cache' | 'other';
 export interface Answer {
     source?: SourceType;
