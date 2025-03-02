0a5ec62661d298e366a1167eee67f2bb92d04f11
mdedirudianto <mdedirudianto@gmail.com>
2025-03-02 17:07:34 +0700

    feat: audio for full chapter

--- src/bot/verse.ts
+++ src/bot/verse.ts
@@ -12,6 +12,12 @@ function formatAudioLink(chapter: number, verse: number): string {
   return `https://everyayah.com/data/Alafasy_64kbps/${chapterStr}${verseStr}.mp3`;
 }
 
+function formatChapterAudioLink(chapter: number): string {
+  const chapterStr = chapter.toString().padStart(3, '0');
+
+  return `https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/${chapterStr}.mp3`;
+}
+
 export function getVerseRange(
   chapterNo: number,
   verseStart: number,
@@ -32,9 +38,12 @@ export function getVerseRange(
       v.chapter === chapterNo && v.verse >= verseStart && v.verse <= verseEnd
   );
 
-  const audios = verses.map((verse) =>
-    formatAudioLink(verse.chapter, verse.verse)
-  );
+  let audios: string[];
+  if (verseStart === 1 && verseEnd === meta.chapters[chapterNo - 1].verses) {
+    audios = [formatChapterAudioLink(chapterNo)];
+  } else {
+    audios = verses.map((verse) => formatAudioLink(verse.chapter, verse.verse));
+  }
 
   return {
     source: 'quran',
