f8c92668813d82a31362cc075ad4d0631d441d24
mdedirudianto <mdedirudianto@gmail.com>
2025-03-04 15:54:38 +0700

    feat: move audio generator

--- src/bot/verse.ts
+++ src/bot/verse.ts
@@ -2,21 +2,9 @@ import { loadQuran } from 'qute-corpus';
 
 import { debug } from '../logger';
 import { ActionType, Answer } from '../models';
+import { formatAudioLink } from './utils';
 
 const { ar, id, meta } = loadQuran();
-
-function formatAudioLink(chapter: number, verse: number): string {
-  const chapterStr = chapter.toString().padStart(3, '0');
-  const verseStr = verse.toString().padStart(3, '0');
-
-  return `https://everyayah.com/data/Alafasy_64kbps/${chapterStr}${verseStr}.mp3`;
-}
-
-function formatChapterAudioLink(chapter: number): string {
-  const chapterStr = chapter.toString().padStart(3, '0');
-
-  return `https://download.quranicaudio.com/quran/mishaari_raashid_al_3afaasee/${chapterStr}.mp3`;
-}
 
 export function getVerseRange(
   chapterNo: number,
@@ -38,13 +26,9 @@ export function getVerseRange(
       v.chapter === chapterNo && v.verse >= verseStart && v.verse <= verseEnd
   );
 
-  let audios: string[];
-
-  if (verseStart === 1 && verseEnd === meta.chapters[chapterNo - 1].verses) {
-    audios = [formatChapterAudioLink(chapterNo)];
-  } else {
-    audios = verses.map((verse) => formatAudioLink(verse.chapter, verse.verse));
-  }
+  const audios = verses.map((verse) =>
+    formatAudioLink(verse.chapter, verse.verse)
+  );
 
   return {
     source: 'quran',
