3995ecd7d0365202011549d8e20d38013babbdd2
mdedirudianto <mdedirudianto@gmail.com>
2025-03-04 17:01:03 +0700

    chore: build libs

---
+++ lib/bot/utils.d.ts
@@ -0,0 +1,3 @@
+export declare function formatAudioLink(chapter: number, verse: number): string;
+export declare function formatChapterAudioLink(chapter: number): string;
+//# sourceMappingURL=utils.d.ts.map
